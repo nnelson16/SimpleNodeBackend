831f8b324c14fa78047b480af6ae9feb07481c80
nnelson16 <nn167@scarletmail.rutgers.edu>
2017-12-10 23:14:54 -0500

    Can update and delete notes as well

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -26,4 +26,31 @@ module.exports = function(app, db) {
       }
     });
   });
+
+  //delete a note
+  app.delete('/notes/:id', function(req, res) {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, function(err, item) {
+        if(err) {
+          res.send({ 'error': 'An error has occured'});
+        } else {
+          res.send('Note '+ id + ' deleted');
+        }
+    });
+  });
+
+  //update a note
+  app.put('/notes/:id', function(req, res) {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, function(err, result) {
+      if(err) {
+        res.send({'error': 'An error has ocurred'});
+      } else {
+        res.send(note);
+      }
+    });
+  });
 };
