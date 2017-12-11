8f82dd78500e500125cd2da0a86b18b8e8dc2b08
nnelson16 <nn167@scarletmail.rutgers.edu>
2017-12-10 22:48:39 -0500

    Get requests for notes objects work

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,20 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+
+  //get a note based on id
+  app.get('/notes/:id', function(req, res){
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id)};
+      db.collection('notes').findOne(details, function(err, item) {
+        if(err) {
+          res.send({ 'error': 'An error has ocurred'});
+        } else {
+          res.send(item);
+        }
+      });
+  });
+
   app.post('/notes', function(req, res) {
     //note creation happens here
     const note = { text: req.body.body, title: req.body.title };
