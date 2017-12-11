d7519ea4c762dfd48a99ceb7170196d03173c52d
nnelson16 <nn167@scarletmail.rutgers.edu>
2017-12-10 22:12:01 -0500

    Mongo DB setup complete

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-// require dependincies
+// require dependencies
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db.js')
 
 const app = express();
 
@@ -9,7 +10,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 8000;
 
-require('./app/routes')(app, {});
-app.listen(port, function() {
-  console.log('We are live on port: '+port);
+MongoClient.connect(db.url, function(err, database) {
+  if(err) {
+    return console.log(err);
+  }
+  require('./app/routes')(app, database);
+
+  app.listen(port, function() {
+    console.log('We are live on port: '+port);
+  });
 });
